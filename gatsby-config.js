c9f5c5aec92cf9dcd71719ca16754ebb0d64411a
Helgi Helgason <helgihel@gmail.com>
2020-09-04 13:40:24 +0000

    add redirecs file

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify`,
     {
       resolve: "gatsby-source-sanity",
       options: {
